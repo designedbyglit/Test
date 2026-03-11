b4fb201219b56295e49f36d79961ae873cfbff8a
designedbyglit <afbgraphics45230@gmail.com>
2026-03-11 23:40:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 
+/* ANIMATION SCROLL PRODUITS */
+
+const produits = document.querySelectorAll(".produit")
+
+window.addEventListener("scroll", () => {
+
+produits.forEach((p) => {
+
+const position = p.getBoundingClientRect().top
+const screenPosition = window.innerHeight / 1.2
+
+if(position < screenPosition){
+
+p.classList.add("visible")
+
+}
+
+})
+
+})
+
+
+
+/* PANIER SIMPLE */
+
+let panier = []
+
+function ajouterPanier(nom, prix){
+
+panier.push({
+nom: nom,
+prix: prix
+})
+
+alert("Produit ajouté au panier")
+
+console.log(panier)
+
+}
+
