21c5bc2b2ce65050f1e73314dfd6da4cc39c90ea
designedbyglit <afbgraphics45230@gmail.com>
2026-03-13 21:01:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+function addToCart(name,price){
+
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+cart.push({
+name:name,
+price:price
+})
+
+localStorage.setItem("cart",JSON.stringify(cart))
+
+alert("Produit ajouté au panier")
+
+}
+
+function openPanier(){
+
+window.location.href="panier.html"
+
+}
